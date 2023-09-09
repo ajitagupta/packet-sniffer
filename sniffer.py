14d2cc5b4670ab217bc588789a2d34eb94106f2f
Ajita Gupta <ajita.gupta001@gmail.com>
2023-09-09 12:00:37 +0530

    Create sniffer.py

---
+++ sniffer.py
@@ -0,0 +1,12 @@
+import socket
+import struct
+import binascii
+
+s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket. htons(0x0800)) # creating socket
+while True:
+    packet = s.recvfrom(2048)
+
+# a bit more criminology
+ethernet_header = packet[0][0:14] # Get Ethernet Header
+eth_header = struct.unpack("!6s6s2s", ethernet_header)
+print ("Destination MAC:" + binascii.hexlify(eth_header[0]) + " Source MAC:" + binascii.hexlify(eth_header[1]))) # finding the crook
